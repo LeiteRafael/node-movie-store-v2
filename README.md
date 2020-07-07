d7eecaf38f02fea1cb8e01dd7a0afebcc50e8d83
Rafael Leite <rafanleite@gmail.com>
2020-07-06 23:13:10 -0300

    Ajustes no Controller e na documentacao da API

--- README.md
+++ README.md
@@ -1,5 +1,175 @@
 # Locadora De Filmes
 
-
 > O sistema permite a criação de usuários (clientes), logon e logoff de um usuário, listagem de filmes disponíveis, locação de um filme, devolução de um filme, e pesquisa de filme pelo título.
+
+---
+
+### Dependências
+
+- bcrypt
+- Sequelize
+- express
+- express-session
+- mysql
+
+### Conceitos
+
+- Session para Login/Logoff
+- Bcrypt para senha do usuário
+- Destructuring assignment (ES6 )
+- Padrao de projeto: Models & Controllers
+- Querystring para filtro
+
+## Rotas
+
+- ### Create User
+  > |POST| localhost:5555/users
+
+#### Body
+
+```
+{
+	"nome": "nome",
+	"email" : "email",
+	"senha" : "senha"
+}
+```
+
+#### Response
+
+```
+{
+  "msg": "Usuario cadastrado com sucesso"
+}
+```
+
+---
+
+- ### Login
+  > |POST| localhost:5555/users/login
+
+#### Body
+
+```
+{
+	"email" : "email",
+	"password" : "password"
+}
+```
+
+#### Response
+
+```
+{
+  "msg": "Login efetuado com sucesso"
+}
+```
+
+---
+
+- ### Availables
+  > |GET| localhost:5555/movies/availables
+
+#### Response
+
+```
+{
+  "item": [
+    {
+      "title": "Sonic: O Filme",
+      "director": "Jeff Fowler",
+      "idmovie": 1
+    },
+    {
+      "title": "Velozes e Furiosos 1",
+      "director": "Justin Lin",
+      "idmovie": 2
+    },
+    {
+      "title": "Velozes e Furiosos 2",
+      "director": "Justin Lin",
+      "idmovie": 3
+    },
+  ]
+}
+```
+
+---
+
+- ### Find 
+  > |GET| localhost:5555/movies?director=Justin Lin
+
+#### Response
+
+```
+{
+  "item": [
+    {
+      "title": "Velozes e Furiosos 1",
+      "director": "Justin Lin",
+      "idmovie": 2
+    },
+    {
+      "title": "Velozes e Furiosos 2",
+      "director": "Justin Lin",
+      "idmovie": 3
+    },
+    {
+      "title": "Velozes e Furiosos 3",
+      "director": "Justin Lin",
+      "idmovie": 4
+    }
+  ]
+}
+```
+
+---
+
+- ### Rent Movie
+  > |POST| localhost:5555/rents
+
+
+#### Body
+
+```
+{
+	"idmovie":"8"
+}
+```
+
+
+#### Response
+
+```
+{
+  "msg": "Filme alugado"
+}
+```
+
+---
+
+- ### Return Movie
+  > |PATCH| localhost:5555/rents/84/movie/8
+
+#### Response
+
+```
+{
+  "msg": "Filme Devolvido"
+}
+```
+
+---
+
+- ### Logoff
+  > |GET| localhost:5555/users/logout
+
+#### Response
+
+```
+{
+  "msg": "Logoff realizado"
+}
+```
+
 ---
--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -6,40 +6,53 @@ module.exports = {
   async create(request, response) {
     const { name, email, password } = request.body;
     const hash = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, password: hash, email });
-    return response.json(user);
+    await User.create({ name, password: hash, email })
+      .then(() => {
+        return response.json({ msg: "Usuario cadastrado com sucesso" });
+      })
+      .catch((err) => {
+        console.log(err);
+        return response.status(400).json({ msg: "Parametros invalidos" });
+      });
   },
+
   async login(request, response) {
     const { email, password } = request.body;
-    const user = await User.findAll({
+    await User.findAll({
       where: {
         email: email,
       },
-    });
-
-    if (user != "") {
-      const compare = user[0]["dataValues"]["password"];
-      bcrypt.compare(password, compare).then(function (res) {
-        if (res) {
-          sessionLogin = request.session;
-          sessionLogin.user = {};
-          sessionLogin.user.id =user[0]["dataValues"]["id"];
-          return response.json({ msg: "Login efetuado com sucesso" });
+    })
+      .then((item) => {
+        if (item != "") {
+          const compare = item[0]["dataValues"]["password"];
+          bcrypt.compare(password, compare).then(function (res) {
+            if (res) {
+              sessionLogin = request.session;
+              sessionLogin.user = {};
+              sessionLogin.user.id = item[0]["dataValues"]["id"];
+              return response.json({ msg: "Login efetuado com sucesso" });
+            } else {
+              return response.status(401).json({ msg: "Senha incorreta " });
+            }
+          });
         } else {
-          return response.json({ msg: "Senha incorreta " });
+          return response.json({
+            msg: "Usuario nao encontrado, verifiquei as credencias",
+          });
         }
+      })
+      .catch((err) => {
+        console.log(err);
+        return response.status(400).json({ msg: "Parametros invalidos" });
       });
-    } else {
-      return response.json({
-        msg: "Usuario nao encontrado, verifiquei as credencias",
-      });
-    }
   },
+
   async logout(request, response) {
     const sessionLogin = request.session;
     sessionLogin.destroy(function (err) {
-        if (err) response.json({ "msg": "Erro ao destruir a Session" });
-        response.json({"msg":"Logoff realizado"});
+      if (err) response.status(400).json({ msg: "Erro ao destruir a Session" });
+      response.json({ msg: "Logoff realizado" });
     });
-}
+  },
 };
